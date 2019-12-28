788137fcc637a8a268497727893ca2a82c05d094
Paul Spain <pvspain@gmail.com>
2019-12-28 13:02:08 +1100

    Preserve and subsequently reapply any local changes via stash

--- Invoke-UpstreamSync.ps1
+++ Invoke-UpstreamSync.ps1
@@ -5,7 +5,7 @@ $upstreamRepoUrl = 'https://github.com/RamblingCookieMonster/Invoke-Parallel'
 Push-Location $PSScriptRoot | Out-Null
 
 # Create upstream remote if absent...
-if (-not (& git remote | Where-Object {$_ -match 'upstream' })) {
+if (-not (& git remote | Where-Object { $_ -eq 'upstream' })) {
   Write-Output "Creating remote 'upstream'..."  
   & git remote add upstream $upstreamRepoUrl
 } 
@@ -15,10 +15,14 @@ if (& git remote | Where-Object {$_ -match 'upstream' }) {
     & git fetch upstream
     Write-Output "Checking out local 'master' branch..."  
     & git checkout master
+    Write-Output "Stashing local changes..."  
+    & git stash push
     Write-Output "Merging changes from 'upstream/master'..."  
     & git merge upstream/master
     Write-Output "Pushing changes to 'origin/master'..."  
     & git push
+    Write-Output "Unstashing local changes..."  
+    & git stash pop
 }
 
 Pop-Location | Out-Null
